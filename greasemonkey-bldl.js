8eb581189ed956361a455bf3c0776c5e9e0a3774
Simon-CHOU <mrsimonzhou@gmail.com>
2023-12-24 12:13:23 +0800

    init bilibili download grease monkey scirpt

---
+++ greasemonkey-bldl.js
@@ -0,0 +1,106 @@
+// ==UserScript==
+// @name     快速复制B站视频元信息
+// @version  1
+// @grant    none
+// ==/UserScript==
+// 例如，延迟 2 秒后执行（避免油猴脚本创建div不生效）
+console.log('进入猫抓油猴脚本-等待开始')
+setTimeout(function () {
+  console.log('进入猫抓油猴脚本-等待结束')
+    // 在这里编写创建 div 的代码
+
+    var currentUrl = window.location.href;
+    var regex = /video\/(.*?)\//; 
+    var match = currentUrl.match(regex);
+    if (match) { var extractedSubstring = match[1]; console.log(extractedSubstring); } else { console.log("未找到匹配的子串（BV号)"); }
+  
+
+    if (currentUrl.includes('www.bilibili.com/video')) {
+        // 获取ID为"share-btn-outer"的元素
+        var shareBtnOuter = document.getElementById('share-btn-outer');
+
+        // 创建一个新的button元素
+        var newButton = document.createElement('button');
+
+        // 设置button的文本为"下载"
+        newButton.textContent = '下载';
+
+        // 为button添加点击事件监听器
+        newButton.addEventListener('click', function () {
+
+            console.log('当前处在B站视频页')
+            //up主名称
+            var k = document.getElementsByClassName("up-name")
+            if (k[0] === undefined) { // 联合投稿
+                k = document.getElementsByClassName("staff-name is-vip")
+            }
+            var upname = k[0].innerText
+
+            //视频标题
+            var t = document.getElementsByClassName("video-title")
+            var title = t[0].innerText
+            //发布时间
+            var r = document.getElementsByClassName("pubdate-text")
+            var date = r[0].innerText
+            var datef = date.substring(0, 10)
+
+            //BV号
+            var bv = window.location.href.match( /video\/(.*?)\//)[1];
+
+            //拼接出文件名。
+            var output = title.concat('_', datef, '_@', upname, '_', bv)
+            console.log(output)
+            navigator.clipboard.writeText(output)
+                .then(function () {
+                    console.log('复制成功！');
+                })
+                .catch(function (err) {
+                    console.error('复制失败：', err);
+                });
+
+        });
+
+        // 将新创建的button元素添加到shareBtnOuter元素之后
+        shareBtnOuter.insertAdjacentElement('afterend', newButton);
+      
+       var newButtonWithUrl = document.createElement('button');
+
+        // 设置button的文本为"下载"
+        newButtonWithUrl.textContent = '下载带URL';
+        newButtonWithUrl.addEventListener('click', function () {
+
+            console.log('当前处在B站视频页')
+            //up主名称
+            var k = document.getElementsByClassName("up-name")
+            if (k[0] === undefined) { // 联合投稿
+                k = document.getElementsByClassName("staff-name is-vip")
+            }
+            var upname = k[0].innerText
+
+            //视频标题
+            var t = document.getElementsByClassName("video-title")
+            var title = t[0].innerText
+            //发布时间
+            var r = document.getElementsByClassName("pubdate-text")
+            var date = r[0].innerText
+            var datef = date.substring(0, 10)
+
+            //BV号
+            var bv = window.location.href.match( /video\/(.*?)\//)[1];
+
+            //拼接出文件名。
+            var output = title.concat('_', datef, '_@', upname, '_', bv)
+            var outputUrl = output.concat('\r\n', 'https://www.bilibili.com/video/', bv)
+            console.log(outputUrl)
+            navigator.clipboard.writeText(outputUrl)
+                .then(function () {
+                    console.log('复制成功！');
+                })
+                .catch(function (err) {
+                    console.error('复制失败：', err);
+                });
+
+        });
+       shareBtnOuter.insertAdjacentElement('afterend', newButtonWithUrl);
+    }
+}, 3250);
